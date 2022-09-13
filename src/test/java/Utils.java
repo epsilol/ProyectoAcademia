d2011a2d2eb4f0f34b2545584ca3f34d4c857baa
jose.vazquez.coello <jmvcoello@gmail.com>
2022-09-13 15:18:09 -0500

    Updated Utils.java with USER INFO

--- src/test/java/Utils.java
+++ src/test/java/Utils.java
@@ -3,4 +3,21 @@
 public class Utils {
     final static String BASE_URL = "https://formy-project.herokuapp.com/form";
     final static String CHROME_DRIVER_LOCATION = "chromedriver";
+
+    /* USER INFO
+    E-Mail: CarlosNoEsta@hotmail.com
+    Contraseña: 1234qwer
+    First Name: Carlos
+    Last Name: Sangron
+    Company: Accenture
+    Address: Calle Flor 5258, Colonia Florida
+    City: Monterrey
+    State: Alabama
+    Zip/Postal Code: 49100
+    Country: United States
+    Mobile Phone: 1234567890
+    Assign an adress alias for future reference: Casa
+     */
+
+
 }

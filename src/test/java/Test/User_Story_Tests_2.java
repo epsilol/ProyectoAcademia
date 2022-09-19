21266771f28544b29c5717fc7873726796a8e2eb
j.gonzalez.cabrera <Josue_Gonzalez_Cabrera@hotmail.com>
2022-09-19 09:21:14 -0700

    I added the preparations for the test 2

---
+++ src/test/java/Test/User_Story_Tests_2.java
@@ -0,0 +1,33 @@
+package Test;
+
+import Pages.Home_Page;
+import Steps.User_Story_Step_1;
+import Utilerias.Utils;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeSuite;
+import org.testng.annotations.Test;
+
+public class User_Story_Tests_2 {
+    public static final WebDriver driver = new ChromeDriver();
+
+    @BeforeSuite
+    public static void main(String[] args) {
+        // ChromeDriver location set up in Utilerias.Utils class
+        System.setProperty("webdriver.chrome.driver", Utils.CHROME_DRIVER_LOCATION);
+    }
+
+    @Test(testName = "Page Navigation")
+    public static void navigation(){
+        driver.get(Utils.BASE_URL);
+        Home_Page Home_Page = new Home_Page(driver);
+
+    }
+
+    @AfterSuite
+    public static void cleanUp(){
+        driver.manage().deleteAllCookies();
+        driver.close();
+    }
+}

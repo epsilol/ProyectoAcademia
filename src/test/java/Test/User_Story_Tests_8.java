f53e76cdf0cd1779f0a7855d8da168cf05daf1db
a.villarreal.arriaga <ac.viar92@gmail.com>
2022-09-20 16:05:21 -0500

    R 8.1 - Contact customer support 1.1

--- src/test/java/Test/User_Story_Tests_8.java
+++ src/test/java/Test/User_Story_Tests_8.java
@@ -17,7 +17,6 @@ import org.testng.annotations.BeforeSuite;
 import org.testng.annotations.Test;
 
 public class User_Story_Tests_8 {
-
     public static final WebDriver driver = new ChromeDriver();
 
     @BeforeSuite
@@ -26,17 +25,17 @@ public class User_Story_Tests_8 {
             System.setProperty("webdriver.chrome.driver", Utils.CHROME_DRIVER_LOCATION);
     }
 
-    @Test(testName="R8.1 Contactcustomersupport")//poneraquidequesetratacasodeprueba
-    publicstaticvoidContactCustomerSupport(){//submitformcambiarnombredefuncionNOLLAMARSEIGUAL
+    @Test(testName="R8.1 Contact customer support")//poner aqui de que se trata caso de prueba
+    public static void ContactCustomerSupport(){
         driver.get(Utils.BASE_URL);
-        Home_Page Home_Page= new Home_Page(driver);
+        Home_Page Home_Page = new Home_Page(driver);
         Login Login = new Login(driver);
-        //ContactUsContact_Us=newContactUs(driver);
+        //ContactUs Contact_Us = new ContactUs(driver);
         User_Story_Step_8 ContactUs = new User_Story_Step_8();
         ContactUs.ClickContactUs();
         ContactUs.HeadingDropDown();
         WebDriverWait wait = new WebDriverWait(driver,100);
-        WebElement DropDown = driver.findElement(By.id(("id_contact"));
+        WebElement DropDown = driver.findElement(By.id("id_contact"));
         Select select = new Select(DropDown);
         select.selectByValue("2");
         ContactUs.ContactUsEmail();
@@ -46,7 +45,7 @@ public class User_Story_Tests_8 {
     }
 
     @AfterSuite
-    publicstaticvoidcleanUp(){
+    public static void cleanUp(){
         driver.manage().deleteAllCookies();
         driver.close();
     }

0c88fd7968c883347f1b1d8573dcb55d732f7aa7
LuisUribe95 <uribe_morelos@hotmail.com>
2022-09-20 13:03:49 -0500

    Adding the stories of the SHOPPING CART

--- src/test/java/Pages/Shopping_Cart.java
+++ src/test/java/Pages/Shopping_Cart.java
@@ -5,6 +5,8 @@ import Utilerias.PageObject;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 
+import javax.xml.xpath.XPath;
+
 public class Shopping_Cart extends  PageObject{
 
     public Shopping_Cart(WebDriver driver) {
@@ -12,21 +14,41 @@ public class Shopping_Cart extends  PageObject{
     }
 
 
+    @FindBy(xpath = "//*[@id=\"header_logo\"]/a/img")
+    public static  WebElement Logo;
     @FindBy(xpath = "//*[@id=\"header\"]/div[3]/div/div/div[3]/div/a")
     public static WebElement buttonCart;
 
-    //XPATH COMPLETO
     @FindBy(xpath = "//*[@id=\"homefeatured\"]/li[1]/div/div[2]/div[2]/a[1]")
     public static WebElement AddItem;
 
+    @FindBy(xpath = "//*[@id=\"homefeatured\"]/li[2]/div/div[1]/div/a[1]/img")
+    public static WebElement AddItem2;
 
-    @FindBy(xpath = "//*[@id=\"layer_cart\"]/div[1]/div[1]")
+    @FindBy(xpath = "//*[@id=\"homefeatured\"]/li[3]/div/div[1]/div/a[1]/img")
+    public static WebElement AddItem3;
+
+    @FindBy(xpath = "//*[@id=\"add_to_cart\"]/button/span")
+    public static WebElement ButtonAddCartDetails;
+
+
+    @FindBy(xpath = "//*[@id=\"header\"]/div[3]/div/div/div[5]")
+    public  static WebElement Overlayer;
+
+    @FindBy(xpath = "//*[@id=\"layer_cart\"]")
     public static WebElement AlertSuccess;
 
+    @FindBy(xpath = "//*[@id=\"layer_cart\"]/div[1]/div[1]/span")
+    public static WebElement CloseModalButton;
+
+    @FindBy(id = "1_1_0_0") //Del primer elemento que encuentre en la tabla
+    public static WebElement buttonDelete;
 
 
+    @FindBy(id = "cart_quantity_up_1_1_0_0")//Boton para incrementar las cantidades
+    public static WebElement buttonQuantityUp;
 
-
-
+    @FindBy(id = "cart_quantity_down_1_1_0_0")//Boton para reducir las cantidades
+    public static WebElement buttonQuantityDown;
 
 }

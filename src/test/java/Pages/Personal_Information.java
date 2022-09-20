65ee2671d3a1c9558846603089e272b6eca84ae2
rodri113 <rodrigo.t.g.11@hotmail.com>
2022-09-20 14:24:10 -0500

    Creacion de Page Personal_Information

---
+++ src/test/java/Pages/Personal_Information.java
@@ -0,0 +1,12 @@
+package Pages;
+
+import org.openqa.selenium.WebDriver;
+import Utilerias.PageObject;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+public class Personal_Information extends PageObject {
+
+
+
+}
